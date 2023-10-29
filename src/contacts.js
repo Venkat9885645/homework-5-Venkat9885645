8c139ea3b1d13e8d79823ecc24e7327d64bf36a2
venkatesh jagarlamudi <146375020+Venkat9885645@users.noreply.github.com>
2023-10-29 01:08:39 -0400

    Update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -1,34 +1,34 @@
-const contacts = [
-    {
-      name: "Shana loveless",
-      imgURL: "https://media.licdn.com/dms/image/C4D03AQFIUg-HgDByvw/profile-displayphoto-shrink_400_400/0/1600262430813?e=1703721600&v=beta&t=wgVZcqcvwtnoHIRQWQ_mV0dJY4_EaChSwvq38f2CILE",
-      phone: "+1 (614) 947-6199",
-      email: "shana.loveless@franklin.edu"
-    },
-    {
-      name: "Kemal Adyin",
-      imgURL: "https://media.licdn.com/dms/image/C4E03AQE-40IRli5Frg/profile-displayphoto-shrink_800_800/0/1540920894374?e=1703721600&v=beta&t=vJmljvnISzOp9d9dUDx2i5AXUf9FptQgRQRluUokOxo",
-      phone: "+1 (501) 442-0274",
-      email: "kemal.aydin@franklin.edu"
-    },
-    {
-      name: "Todd Whittaker",
-      imgURL: "https://media.licdn.com/dms/image/C4E03AQEkp1U0rI4XeQ/profile-displayphoto-shrink_400_400/0/1580497841057?e=1703721600&v=beta&t=9oLJXh1iQ4FMc5j_YY6mCkCx1eTQpFHdp0eG0GMOJe8",
-      phone: "+1(614) 947-6110",
-      email: "todd.whittaker@franklin.edu"
-    },
-    {
-      name: "Michael Klingler",
-      imgURL: "https://media.licdn.com/dms/image/C4E03AQHYKFim-Pq4oA/profile-displayphoto-shrink_800_800/0/1603733317654?e=1703721600&v=beta&t=xxU04kR70J7dOieP6LJlmmf3J2oxHzWOX2gE_i4DeW4",
-      phone: "+1 (614) 947-6151",
-      email: "michael.klingler@franklin.edu"
-    },
-    {
-      name: "David Decker",
-      imgURL: "https://media.licdn.com/dms/image/C4E03AQGZdhb-HdO9aA/profile-displayphoto-shrink_800_800/0/1540080966923?e=1703721600&v=beta&t=1VYtaS_Hzi92sLAbNP_7Ry3fU1mM6lkkZmnNtMUsZnw",
-      phone: "+1(614) 947-6020",
-      email: "david.decker@franklin.edu"
-    }
-  ];
-  
-  export default contacts;
+import React from "react";
+import PropTypes from "prop-types";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.img} alt="avatar_img" />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+        
+      </div>
+      <a
+          href={props.linkedin}
+          target="_blank"
+        >
+          LinkedIn profile
+        </a>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  tel: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
+};
+
+export default Card;
